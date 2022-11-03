17f79b10a6fbecfc1a2a77ccb74c18bc74292fe6
bibliobee <97406629+bibliobee@users.noreply.github.com>
2022-11-03 09:28:35 -0700

    made some edits in JS and CSS

--- src/app.js
+++ src/app.js
@@ -4,81 +4,102 @@ function displayDate() {
   let date = new Date(timestamp);
   let hours = date.getHours;
   let minutes = date.getMinutes;
-  if (minutes<10) {
+  if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
-let days = [
-"Sunday",
- "Monday",
-"Tuesday",
-"Wednesday",
-"Thursday",
-"Friday",
-"Saturday"
-];
-let day = days[date.getDay()];
-return `${day} ${hours}:${minutes};
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  humidityElement.innerHTML = response.data.main.humidity;
-  precipitationElement.innerHTML = response.data.main.precipitation;
-  windElement.innerHTML = Math.round(response.data.main.wind.speed);
-dateElement.innerHTML = formatDate(response.data.dt *1000);
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
 
-function search(city) {
-let apiKey = "63214c4281922e3bb72fdf12dada7734";
-let apiUrl =
-  "https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={apiKey}&units={metric}";
-  axios.get(url).then(displayTemperature);
+  let forecastHTML = "";
 
-  function displayTemperature(response) {
+  forecastHTML = `
+  <div class="row">
+            <div class="col-xl-6">
+              <div class="icon">
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                  alt="Clear"
+                />
+                </div>
+             <div class="temperature"></div>
+                12° <span class="celsius"><span class="celsius-link">C</span></span> | °<span class="fahrenheit"><span class="fahrenheit-link"><a href="">F</a></span></span>
+              </div>
+            </div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let precipitationElement = document.querySelector("#precipitation");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-let windElement = document.querySelector("#wind);
-let cityElement = document.querySelector("#city");
-dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let windElement = document.querySelector("#wind");
+  let cityElement = document.querySelector("#city");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+humidityElement.innerHTML = response.data.main.humidity;
+precipitationElement.innerHTML = response.data.main.precipitation;
+windElement.innerHTML = Math.round(response.data.main.wind.speed);
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
-  function handleSubmit(event) {
-    event.preventDefault;
-let cityInputElement = document.querySelector("#city-input");
-console.log(cityInputElement.value);
-search(cityInputElement.value);
-  }
+function search(city) {
+  let apiKey = "63214c4281922e3bb72fdf12dada7734";
+  let apiUrl =
+    "https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={apiKey}&units={metric}";
+  axios.get(url).then(displayTemperature);
+}
 
-  function displayFahrenheitTemp(event) {
-    event.preventDefault();
-    let fahrenheitElement = document.querySelector("#fahrenheit");
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
-    temperatureElement.innerHTML = Math.round(fahrenheitTemp);
-  }
+function handleSubmit(event) {
+  event.preventDefault;
+  let cityInputElement = document.querySelector("#city-input");
+  console.log(cityInputElement.value);
+  search(cityInputElement.value);
+}
 
-  function displayCelsiusTemp(event) {
-    event.preventDefault();
-        celsiusLink.classList.radd("active");
-    fahrenheitLink.classList.remove("active");
-    let temperatureElement = document.querySelector("#celsius");
-    temperatureElement.innerHTML = celsiusTemp;
-  }
-  
-  let celsiusTemp = null;
-  
-  let form = document.querySelector(#search-form);
-  form.addEventListener("submit", handleSubmit);
+function displayFahrenheitTemp(event) {
+  event.preventDefault();
+  let fahrenheitElement = document.querySelector("#fahrenheit");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
 
-  let fahrenheitLink = document.querySelector("#fahrenheit-link");
-  fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+function displayCelsiusTemp(event) {
+  event.preventDefault();
+  celsiusLink.classList.radd("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#celsius");
+  temperatureElement.innerHTML = celsiusTemp;
+}
 
-  let celsiusLink = document.querySelector("#celsius-link");
-  celsiusLink.addEventListener("click", displayCelsiusTemp);
+let celsiusTemp = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemp);
